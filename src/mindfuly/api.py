76ba9611016235a392185dbd2a183a77b6f9dce5
nnull. <jovkun.grad2023@my.stmc.bc.ca>
2025-11-12 15:45:23 -0800

    barebone ui for home, login, and signup

--- src/mindfuly/api.py
+++ src/mindfuly/api.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from src.mindfuly.routes import authorization, users, mood, spotify, weather
 
+from index.main import ui
+
 app = FastAPI(
     title="Mindfuly",
     version="1.0.0",
     decription="Handles mood logs, Spotify sesssions, weather context, and user authentication",
 )
 
-
 app.include_router(authorization.router)
 app.include_router(users.router)
 app.include_router(mood.router)
 app.include_router(spotify.router)
 app.include_router(weather.router)
+
+ui.run_with(app, mount_path="/", favicon="💭", title="Mindfuly")
